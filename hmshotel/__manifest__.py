393500a5951f96882f8a2e51d13922b6e0d162a1
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-17 10:01:49 +0630

    nthing

--- hmshotel/__manifest__.py
+++ hmshotel/__manifest__.py
@@ -25,9 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        # 'views/views.xml',
         # 'views/templates.xml',
-        # 'views/hotel_view.xml'
         'views/view.xml'
     ],
     # only loaded in demonstration mode
