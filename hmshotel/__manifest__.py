ffa18e2f2e3d0563448cb52dfb63bb80c2f8f856
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-17 08:58:21 +0630

    .py test again

--- hmshotel/__manifest__.py
+++ hmshotel/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         # 'views/views.xml',
         # 'views/templates.xml',
-        'views/hotel_view.xml'
+        # 'views/hotel_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
