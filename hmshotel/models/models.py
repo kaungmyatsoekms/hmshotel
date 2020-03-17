9a1381c82ac223cfc215c9496444a20e3d9d4aa4
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-17 11:19:56 +0630

    new class : floor

--- hmshotel/models/models.py
+++ hmshotel/models/models.py
@@ -16,3 +16,12 @@ class hmshotel(models.Model):
     # def _value_pc(self):
     #     for record in self:
     #         record.value2 = float(record.value) / 100
+
+class HotelFloor(models.Model):
+    _name = 'hotel.floor'
+    _description = 'Floor'
+
+    name = fields.Char('Floor Name', size=64, required=True, index=True)
+    sequence = fields.Integer('Sequence', size=64, index=True)
+
+
