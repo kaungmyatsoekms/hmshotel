2deed7eeeef16a7f82ec1617308a4649990550a3
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-19 12:17:16 +0630

    change text

--- hmshotel/models/models.py
+++ hmshotel/models/models.py
@@ -21,23 +21,6 @@ class HotelFloor(models.Model):
     _name = 'hotel.floor'
     _description = 'Floor'
 
-    name = fields.Char('Floor Name', size=5, required=True, index=True)
-    sequence = fields.Integer('Sequence', size=64, index=True)
-
-
-# class HotelRoomType(models.Model):
-#     _name - 'hotel.room.type'
-#     _description = 'Roome Type'
-
-#     name = fields.Char('Name', size = 64, required = True )
-#     categ_id = fields.Many2one('hotel.room.type', 'Category')
-#     child_id = fields.One2many('hotel.room.type', 'categ_id','Child Categories')
-
-
-class property(models.Model):
-    _name = 'property.property'
-    _description = 'property.property'
-
     property_code = fields.Char()
     property_name = fields.Char()
     address1 = fields.Char()
@@ -56,3 +39,36 @@ class property(models.Model):
     property_rating = fields.Char()
     property_logo = fields.binary()
     property_photo = fields.binary()
+
+
+# class HotelRoomType(models.Model):
+#     _name - 'hotel.room.type'
+#     _description = 'Roome Type'
+
+#     name = fields.Char('Name', size = 64, required = True )
+#     categ_id = fields.Many2one('hotel.room.type', 'Category')
+#     child_id = fields.One2many('hotel.room.type', 'categ_id','Child Categories')
+
+
+# class property(models.Model):
+#     _name = 'property.property'
+#     _description = 'property.property'
+
+#     property_code = fields.Char()
+#     property_name = fields.Char()
+#     address1 = fields.Char()
+#     address2 = fields.Char()
+#     address3 = fields.Char()
+#     country_id = fields.Char()
+#     state_id = fields.char()
+#     city_id = fields.Char()
+#     telephone = fields.Char()
+#     fax = fields.Char()
+#     email = fields.Char()
+#     website = fields.Char()
+#     social_media_links = fields.Char()
+#     no_of_room = fields.Integer(size=4)
+#     proprety_license = fields.Char()
+#     property_rating = fields.Char()
+#     property_logo = fields.binary()
+#     property_photo = fields.binary()
