6b0d612d5ec8b2b4e4434c699eda84548f0f2fb2
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-19 10:08:18 +0630

    new class property

--- hmshotel/models/models.py
+++ hmshotel/models/models.py
@@ -25,23 +25,28 @@ class HotelFloor(models.Model):
     sequence = fields.Integer('Sequence', size=64, index=True)
 
 
-# class property(models.Model):
-#     _name = 'property.property'
-#     _description = 'property.property'
+class property(models.Model):
+    _name = 'property.property'
+    _description = 'property.property'
 
-#     code = fields.Char()
-#     name = fields.Char()
-#     address1 = fields.Char()
-#     address2 = fields.Char()
-#     address3 = fields.Char()
-#     country_id = fields.Char()
-#     state_id = fields.char()
-#     city_id = fields.Char()
-#     telephone = fields.Char()
-#     fax = fields.Char()
-#     email = fields.Char()
-#     website = fields.Char()
-#     social_media_links = fields.Char()
+    code = fields.Char()
+    name = fields.Char()
+    address1 = fields.Char()
+    address2 = fields.Char()
+    address3 = fields.Char()
+    country_id = fields.Char()
+    state_id = fields.char()
+    city_id = fields.Char()
+    telephone = fields.Char()
+    fax = fields.Char()
+    email = fields.Char()
+    website = fields.Char()
+    social_media_links = fields.Char()
+    no_of_romm = fields.Integer(size=4)
+    proprety_license = fields.Char()
+    property_rating = fields.Char()
+    property_logo = fields.binary()
+    property_photo = fields.binary()
 
 
 
