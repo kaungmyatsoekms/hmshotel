988c0231b6ee7e165342d71a3e95ec8580d1f2d8
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-19 12:21:49 +0630

    change text again

--- hmshotel/models/models.py
+++ hmshotel/models/models.py
@@ -21,24 +21,8 @@ class HotelFloor(models.Model):
     _name = 'hotel.floor'
     _description = 'Floor'
 
-    property_code = fields.Char()
-    property_name = fields.Char()
-    address1 = fields.Char()
-    address2 = fields.Char()
-    address3 = fields.Char()
-    country_id = fields.Char()
-    state_id = fields.char()
-    city_id = fields.Char()
-    telephone = fields.Char()
-    fax = fields.Char()
-    email = fields.Char()
-    website = fields.Char()
-    social_media_links = fields.Char()
-    no_of_room = fields.Integer(size=4)
-    proprety_license = fields.Char()
-    property_rating = fields.Char()
-    property_logo = fields.binary()
-    property_photo = fields.binary()
+    name = fields.Char('Floor Name', size=64, required=True, index=True)
+    sequence = fields.Integer('Sequence', size=64, index=True) 
 
 
 # class HotelRoomType(models.Model):
