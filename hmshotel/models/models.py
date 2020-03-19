cde6695f0f958d7fdb072f7303d0c0b14b121a50
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-19 09:54:17 +0630

    size

--- hmshotel/models/models.py
+++ hmshotel/models/models.py
@@ -21,8 +21,28 @@ class HotelFloor(models.Model):
     _name = 'hotel.floor'
     _description = 'Floor'
 
-    name = fields.Char('Floor Name', size=64, required=True, index=True)
+    name = fields.Char('Floor Name', size=5, required=True, index=True)
     sequence = fields.Integer('Sequence', size=64, index=True)
+
+
+# class property(models.Model):
+#     _name = 'property.property'
+#     _description = 'property.property'
+
+#     code = fields.Char()
+#     name = fields.Char()
+#     address1 = fields.Char()
+#     address2 = fields.Char()
+#     address3 = fields.Char()
+#     country_id = fields.Char()
+#     state_id = fields.char()
+#     city_id = fields.Char()
+#     telephone = fields.Char()
+#     fax = fields.Char()
+#     email = fields.Char()
+#     website = fields.Char()
+#     social_media_links = fields.Char()
+
 
 
 # class HotelRoomType(models.Model):
