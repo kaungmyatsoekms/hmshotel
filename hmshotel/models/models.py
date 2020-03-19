45ed0fb2f8394eb0b380b52c56f3d52526a51c24
Kaung Myat Soe <kaungmyatsoe@ZANDO.local>
2020-03-19 12:33:25 +0630

    ddf

--- hmshotel/models/models.py
+++ hmshotel/models/models.py
@@ -34,25 +34,25 @@ class HotelFloor(models.Model):
 #     child_id = fields.One2many('hotel.room.type', 'categ_id','Child Categories')
 
 
-# class property(models.Model):
-#     _name = 'property.property'
-#     _description = 'property.property'
+class property(models.Model):
+    _name = 'property.property'
+    _description = 'property.property'
 
-#     property_code = fields.Char()
-#     property_name = fields.Char()
-#     address1 = fields.Char()
-#     address2 = fields.Char()
-#     address3 = fields.Char()
-#     country_id = fields.Char()
-#     state_id = fields.char()
-#     city_id = fields.Char()
-#     telephone = fields.Char()
-#     fax = fields.Char()
-#     email = fields.Char()
-#     website = fields.Char()
-#     social_media_links = fields.Char()
-#     no_of_room = fields.Integer(size=4)
-#     proprety_license = fields.Char()
-#     property_rating = fields.Char()
-#     property_logo = fields.binary()
-#     property_photo = fields.binary()
+    property_code = fields.Char()
+    property_name = fields.Char()
+    address1 = fields.Char()
+    address2 = fields.Char()
+    address3 = fields.Char()
+    country_id = fields.Char()
+    state_id = fields.char()
+    city_id = fields.Char()
+    telephone = fields.Char()
+    fax = fields.Char()
+    email = fields.Char()
+    website = fields.Char()
+    social_media_links = fields.Char()
+    no_of_room = fields.Integer(size=4)
+    proprety_license = fields.Char()
+    property_rating = fields.Char()
+    property_logo = fields.binary()
+    property_photo = fields.binary()
